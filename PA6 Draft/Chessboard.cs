1f097a4ff542729de9a42f58aff0e05da2a68879
Yasiel <yasiel2110@gmail.com>
2020-11-21 15:14:14 -0500

    begining

--- PA6 Draft/Chessboard.cs
+++ PA6 Draft/Chessboard.cs
@@ -21,6 +21,7 @@ namespace PA6_Draft
         private Point PickedLocation;
         private Dictionary<Piece,Bitmap> PieceImages;//BlackPawn,WhitePawn,BlackRook,WhiteRook,BlackKnight,WhiteKnight,BlackBishop,WhiteBishop
                                                      //,BlackKing, WhiteKing, BlackQueen, WhiteQueen;
+        
         BindingList<Move> bList = new BindingList<Move>();
 
         internal Chessboard(Color Light, Color Dark, ChessGame Game)

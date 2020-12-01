7bcdf1d52047f326a115626e859c9949eb485f3a
Javier Lopez <jlope749@fiu.edu>
2020-12-01 13:46:10 -0500

    Final version

--- PA6 Draft/PromotionForm.Designer.cs
+++ PA6 Draft/PromotionForm.Designer.cs
@@ -81,6 +81,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(120, 286);
             this.Controls.Add(this.KnightButton);
             this.Controls.Add(this.BishopButton);
